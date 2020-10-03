0289ec0e856e742e50d964d8be07a523af6304e8
csehkristofdavid <71436330+csehkristofdavid@users.noreply.github.com>
2020-10-03 10:35:04 +0200

    Add files via upload

---
+++ drill4.cpp
@@ -0,0 +1,60 @@
+#include "std_lib_facilities.h"
+
+double converter(double x, string unit_x){
+	double y=0;
+	if(unit_x=="cm"){
+		y=x/100;
+	}
+	if(unit_x=="in"){
+		y=x*2.54/100;
+	}
+	if(unit_x=="ft"){
+		y=x*12*2.54/100;
+	}
+	if(unit_x=="m"){
+		y=x;
+	}
+	return y;
+}
+int main(){
+
+	double num;
+	double smallest=0;
+	double largest=0;
+	string unit;
+	int i=0;
+	double sum=0;
+	double con_num=0;
+	vector<double> v;
+	while(cin>>num>>unit){
+		if(unit=="m" || unit=="cm" || unit=="in" || unit=="ft"){
+			con_num=converter(num,unit);
+			if(i==0){
+				smallest=con_num; largest=con_num;
+			}
+			if(i>0){
+				if(con_num<smallest){
+					smallest=con_num;
+				}
+				if(con_num>largest){
+					largest=con_num;
+				}
+			}
+			i++;
+			sum+=con_num;
+			v.push_back(con_num);
+		}
+	}
+	cout<<"the smallest: "<<smallest<<" meter(s)"<<endl;
+	cout<<"the largest: "<<largest<<" meter(s)"<<endl;
+	cout<<"the number of values: "<<i<<endl;
+	cout<<"the sum of values: "<<sum<<" meter(s)"<<endl;
+	i=0;
+	cout<<"the elements was/were: ";
+	sort(v);
+	for(i=0;i<v.size();i++){
+		cout<<v[i]<<" m ";
+	}
+	cout<<endl;
+	return 0;
+}
